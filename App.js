b15e692dead02b4a02584a22b6c85d61b395ebf5
Kayke Ragoso <162835740+KaykeRagoso@users.noreply.github.com>
2025-03-07 20:59:52 -0300

    Update App.js

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ export default function App() {
           keyboardType="numeric"
           placeholder="Digite sua senha"
           placeholderTextColor="gray"
-          secureTextEntry // Esconde o texto digitado
+          secureTextEntry // Esconde o texto digitados
         />
       </View>
 
